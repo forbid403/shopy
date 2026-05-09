f02ab92fa7d8513ad767a6bf5051d939646f33a8
forbid403 <forbid403@naver.com>
2026-05-09 14:30:00 +1000

    feat: add auth routes and register with server

--- server/index.ts
+++ server/index.ts
@@ -9,6 +9,7 @@ import productRoutes from './routes/products.js'
 import cartRoutes from './routes/cart.js'
 import favoriteRoutes from './routes/favorites.js'
 import uploadRoutes from './routes/upload.js'
+import authRoutes from './routes/auth.js'
 
 dotenv.config()
 
@@ -23,6 +24,7 @@ app.use(cors())
 app.use(express.json())
 app.use('/uploads', express.static(uploadsDir))
 
+app.use('/api/auth', authRoutes)
 app.use('/api/products', productRoutes)
 app.use('/api/cart', cartRoutes)
 app.use('/api/favorites', favoriteRoutes)
